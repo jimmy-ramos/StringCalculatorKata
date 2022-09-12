8896d27d3dd6eb9e4c9be379c81af70bd70dae8b
JimmyRamos <jramos@lucca.fr>
2022-09-12 16:12:39 +0200

    Adding a test when there is only one number in input

--- StringCalculatorKata/StringCalculatorTest.cs
+++ StringCalculatorKata/StringCalculatorTest.cs
@@ -9,21 +9,25 @@ public class StringCalculatorTest
         
         Assert.Equal(0, actual);
     }
-    
-    [Fact]
-    public void ShouldAddReturn1_WhenInputIs1()
-    {
-        var actual = StringCalculator.Add("1");
-        
-        Assert.Equal(1, actual);
-    }
 
     [Theory]
     [InlineData("1,2", 3)]
     [InlineData("2,6", 8)]
     [InlineData("9,1000", 1009)]
     [InlineData("17,6", 23)]
-    public void ShouldAddReturn8_WhenInputIs2And6(string input, int expected)
+    public void ShouldReturnSum_WhenGivenTwoPositiveNumber(string input, int expected)
+    {
+        var actual = StringCalculator.Add(input);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("17", 17)]
+    [InlineData("3", 3)]
+    [InlineData("520", 520)]
+    public void ShouldReturnNumber_WhenInputIsNumber(string input, int expected)
     {
         var actual = StringCalculator.Add(input);
         
